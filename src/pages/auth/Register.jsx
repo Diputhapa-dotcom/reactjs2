49e7d5144b7f08f0c4adb1c06693e004e2e615ca
Diputhapa-dotcom <magardeepu53@gmail.com>
2026-07-15 06:36:20 +0545

    validation message popup

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import Submit from './Submit'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
+ const navigate =  useNavigate()
   const handleRegister =async (data)=>{
-   await axios.post("https://react30.onrender.com/api/user/register",data);
+try {
+  
+    
+   const response = await axios.post("https://react30.onrender.com/api/user/register",data);
+
+ if(response.status===201){
+   navigate("/login");
+   }else{
+    alert("somthing went wrong");
+   }
+
+
+   
+} catch (error) {
+  alert(error.response.data.message);
+}
+
 
 
   }

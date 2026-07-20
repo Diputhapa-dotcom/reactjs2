c27daabdc7959a099ec2435789c645123b3c1877
Diputhapa-dotcom <magardeepu53@gmail.com>
2026-07-20 12:31:59 +0545

    successfully render

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,8 +1,8 @@
  import React from 'react'
 import Submit from './Submit'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
 import { baseurl } from '../../config'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
@@ -12,11 +12,14 @@ const Login = () => {
     
     
     const response = await axios.post(`${baseurl}/login`,data);
-    if(response.status===200){
-      localStorage.setItem("token",response.data.token);
-    
-    }
   
+   if(response.status===200){
+    {/* server bata pthayeko token data vitra hunxa*/}
+    console.log(response.data); 
+    localStorage.setItem("token",response.data.token)
+   navigate("/home")
+
+   }
 
   }
   return (
